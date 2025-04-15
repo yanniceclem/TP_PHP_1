45dde73d0c22dc6a3f2a7449a33b2e66cbb9cfa3
unknown <clemencinyannice@gmail.com>
2025-04-15 11:06:44 +0200

    ajout de l'exercice

--- index.php
+++ index.php
@@ -8,8 +8,56 @@ class Personne{
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->age = $age;
-    };
+    }
 
+    public function get_nom() {
+        return $this->nom;
+    }
 
+    public function get_prenom() {
+        return $this->prenom;
+    }
 
+    public function get_age() {
+        return $this->age;
+    }
+
+    public function set_nom($nom) {
+        return $this->nom = $nom;
+    }
+
+    public function set_prenom($prenom) {
+        return $this->prenom = $prenom;
+    }
+
+    public function set_age($age) {
+        return $this->age = $age;
+    }
+
+    public function sePresenter() {
+        echo "Bonjour, je m'appelle {$this->prenom} {$this->nom} et j'ai {$this->age} ans.<br>";
+    }
+
+    public function estMajeur() {
+        if ($this->age>=18) {
+            echo "cette personne est majeure.<br>";
+        }
+        else {
+            echo "cette personne est mineur.<br>";
+        }
+    }
 }
+
+$personne1 = new Personne ("clemencin", "yannice", 20);
+
+// $personne1->get_nom();
+// $personne1->get_prenom();
+// $personne1->get_age();
+
+$personne1->sePresenter();
+
+$personne1->estMajeur();
+echo "<br>";
+$personne1->set_age(12);
+$personne1->sePresenter();
+$personne1->estMajeur();
