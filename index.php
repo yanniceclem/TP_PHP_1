bb2b363455cebf69b5f44c50fcb35212d9fb1a3b
unknown <clemencinyannice@gmail.com>
2025-04-17 17:53:21 +0200

    exercice fini

--- index.php
+++ index.php
@@ -10,7 +10,19 @@ class Vehicule {
         $this->annee = $annee;
     }
 
-    public function getInfos() {
+    public function get_marque() {
+        return $this->marque = $marque;
+    }
+
+    public function get_modele() {
+        return $this->modele = $modele;
+    }
+
+    public function get_annee() {
+        return $this->annee = $annee;
+    }
+
+    public function get_infos() {
         return "marque : {$this->marque} modele : {$this->modele} date de sorti : {$this->annee}.<br>";
     }
 
@@ -23,8 +35,45 @@ class Voiture extends Vehicule {
     private $nombrePortes;
     private $typeCarburant;
 
-    public function __construct($nombrePortes, $typeCarburant) {
-        parent::__construct();
+    public function __construct($marque, $modele, $annee, $nombrePortes, $typeCarburant) {
+        parent::__construct($marque, $modele, $annee); 
+        $this->nombrePortes = $nombrePortes;
+        $this->typeCarburant = $typeCarburant;
     }
 
+    public function get_marque() {
+        return $this->marque = $marque;
+    }
+
+    public function get_modele() {
+        return $this->modele = $modele;
+    }
+
+    public function get_annee() {
+        return $this->annee = $annee;
+    }
+
+    public function get_typeCarburant() {
+        return $this->typeCarburant = $typeCarburant;
+    }
+
+    public function get_nombrePortes() {
+        return $this->nombrePortes = $nombrePortes;
+    }
+    
+    public function get_infos() {
+        echo "marque : {$this->marque} modele : {$this->modele} date de sorti : {$this->annee}.<br>";
+    }
+
+    public function demarrer() {
+        echo "Le véhicule démarre.";
+    }
 }
+class Moto extends Vehicule {
+    private $cylindree;
+    
+}
+$voiture = new Voiture("ford", "fiesta", 2013, 5, "Essence");
+
+$voiture->get_infos();
+$voiture->demarrer();
